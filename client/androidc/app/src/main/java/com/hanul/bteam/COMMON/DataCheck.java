259bd1c103fd2479e71696d5901c17be9bb34345
joby <linklifejo@gmail.com>
2023-04-18 09:18:26 +0900

    xxx

--- client/androidc/app/src/main/java/com/hanul/bteam/COMMON/DataCheck.java
+++ client/androidc/app/src/main/java/com/hanul/bteam/COMMON/DataCheck.java
@@ -5,6 +5,23 @@ import com.hanul.bteam.dto.CheckDTO;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.concurrent.Callable;
+import java.util.regex.*;
+
+// 정규식 : import java.util.regex.*;
+//    // 전화번호 체크 예시
+//    String phoneNumber = "010-1234-5678";
+//    boolean isPhoneNumberValid = checkPhoneNumber(phoneNumber);
+//        System.out.println("전화번호 체크 결과: " + isPhoneNumberValid);
+//
+//    // 이메일 주소 체크 예시
+//    String emailAddress = "example@example.com";
+//    boolean isEmailAddressValid = checkEmailAddress(emailAddress);
+//        System.out.println("이메일 주소 체크 결과: " + isEmailAddressValid);
+//
+//    // 영어 문자와 숫자만 허용하며 자리수는 6에서 20자리 체크 예시
+//    String input = "Abc1234";
+//    boolean isInputValid = checkAlphaNumericLength(input);
+//        System.out.println("입력값 체크 결과: " + isInputValid);
 
 public class DataCheck  {
     String tableName = null;
@@ -72,5 +89,44 @@ public class DataCheck  {
         String s = vo.getNull_();
         if(s.equals("N")) return true;
         return false;
+    }
+
+    public static boolean checkPhoneNumber(String phoneNumber) {
+        // 13자리인지 체크
+        if (phoneNumber.length() != 13) {
+            return false;
+        }
+
+        // 숫자인지 체크
+        if (!phoneNumber.matches("[0-9]+")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // 이메일 주소 체크 메소드
+    public static boolean checkEmailAddress(String emailAddress) {
+        // 이메일 형식인지 체크
+        if (!Pattern.matches("[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]+", emailAddress)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // 영어 문자와 숫자만 허용하며 자리수는 6에서 20자리 체크 메소드
+    public static boolean checkAlphaNumericLength(String input) {
+        // 길이가 6에서 20자리인지 체크
+        if (input.length() < 6 || input.length() > 20) {
+            return false;
+        }
+
+        // 영어 문자와 숫자로만 이루어졌는지 체크
+        if (!Pattern.matches("[a-zA-Z0-9]+", input)) {
+            return false;
+        }
+
+        return true;
     }
 }
